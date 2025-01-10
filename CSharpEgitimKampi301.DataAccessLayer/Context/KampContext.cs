6d20a3947e4bb3689ecffc7b1e6b0863de3adf70
Edanur AK <akeda7999@gmail.com>
2025-01-10 17:46:14 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.DataAccessLayer/Context/KampContext.cs
@@ -0,0 +1,19 @@
+﻿using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Context
+{
+    public class KampContext : DbContext//Veritabanına yansıtmak istediğimiz tüm sınıfları KampContext içinde tutuyoruz
+    {
+        public DbSet<Category> Categories { get; set; }//Category class ismi categories veritabanına yasıyacak olan tablo ismi (karışmasın diye)
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }// Navigation property
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }// Navigation property (diğer customer sütunlarını de tutar onlara erişmek için tanımlanır )
+    }
+}
